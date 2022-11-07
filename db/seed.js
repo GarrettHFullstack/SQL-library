fea4f00565040eb41d79f2adb33375f16a62c6c2
GarrHFullstack <Garrett.hall@ttu.edu>
2022-11-07 11:25:39 -0600

    third commit

--- db/seed.js
+++ db/seed.js
@@ -316,17 +316,16 @@ async function getPostById(postId) {
     }
   }
   async function updatePost(postId, fields = {}) {
-    // read off the tags & remove that field 
-    const { tags } = fields; // might be undefined
+
+    const { tags } = fields;
     delete fields.tags;
-  
-    // build the set string
+
     const setString = Object.keys(fields).map(
       (key, index) => `"${ key }"=$${ index + 1 }`
     ).join(', ');
   
     try {
-      // update any fields that need to be updated
+
       if (setString.length > 0) {
         await client.query(`
           UPDATE posts
@@ -336,18 +335,17 @@ async function getPostById(postId) {
         `, Object.values(fields));
       }
   
-      // return early if there's no tags to update
+
       if (tags === undefined) {
         return await getPostById(postId);
       }
-  
-      // make any new tags that need to be made
+
       const tagList = await createTags(tags);
       const tagListIdString = tagList.map(
         tag => `${ tag.id }`
       ).join(', ');
   
-      // delete any post_tags from the database which aren't in that tagList
+
       await client.query(`
         DELETE FROM posts_tags
         WHERE "tagId"
@@ -355,7 +353,7 @@ async function getPostById(postId) {
         AND "postId"=$1;
       `, [postId]);
   
-      // and create post_tags as necessary
+
       await addTagsToPost(postId, tagList);
   
       return await getPostById(postId);
